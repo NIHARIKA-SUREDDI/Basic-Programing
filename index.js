9e58e24954e4a08c2f301cf3cdaf2fcf5442dd18
NIHARIKA-SUREDDI <niharika.sureddi@gmail.com>
2024-12-05 11:45:56 +0530

    Added prime number function

---
+++ index.js
@@ -0,0 +1,12 @@
+// Function to check if a number is prime
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+
+console.log(isPrime(11)); 
+console.log(isPrime(4));  
